85581aadb832587a60da2daf4aba597321ee1fae
myusername <thegoodwizard64@gmail.com>
2020-11-13 21:10:38 -0500

    Inital Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ScoreBoard from '../components/ScoreBoard.js';
+import '../css/App.css';
+
+
+const App = () => (
+  <div className="App">
+    <ScoreBoard />
+  </div>
+)
+
+export default App;
+ 
